a592594a77d9c4612bb88ac9c1773860ae16ffdc
David Jacquel <github@pygmeeweb.com>
2013-10-03 14:05:46 +0200

    js in a file

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
-<script type="text/javascript">
+
+
+// target='_blank' on url that are not par of the current domain
+// see http://stackoverflow.com/questions/4425198/markdown-target-blank
+(function() {
+    var links = document.links;
+    for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+       if (links[i].hostname != window.location.hostname) {
+           links[i].target = '_blank';
+           links[i].className += ' externalLink'
+       }
+    }
+})();
+
   // GOOGLE ANALYTICS CODE
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-157621-1']);
@@ -10,6 +23,8 @@
     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);
   })();
 
+
+
   // DISQUS CODE
   if (typeof disqus_shortname != 'undefined'){
     /* * * DON'T EDIT BELOW THIS LINE * * */
@@ -19,4 +34,3 @@
       (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
     })();
   }
-</script>
